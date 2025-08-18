53be6801820bd5c10412586e5d2063736d081b2a
Brandie Shackelford <brandiebanner@gmail.com>
2025-08-18 10:47:37 -0400

    updated code to fix loading errors

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -3,8 +3,8 @@ nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "income", "res_1980", "res_2002", "age_bir")
 
 library(tidyverse)
-setwd("~/Documents/Teaching/Emory/epi590r-inclass/data/raw/")
-nlsy <- read_csv("nlsy.csv",
+setwd("C:/Users/babanne/OneDrive - Emory/Desktop/EPI590R/epi590r-in-class/data/raw")
+nlsy <- read_csv("nlsy.txt",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
 
