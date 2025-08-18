882a3bdcee095260d74c81e67fee095b2ad4baa4
Brandie Shackelford <brandiebanner@gmail.com>
2025-08-18 14:56:20 -0400

    code for tables

---
+++ R/table1-examples.R
@@ -0,0 +1,82 @@
+library(tidyverse)
+library(gtsummary)
+
+nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
+							 "id", "nsibs", "samp", "race_eth", "sex", "region",
+							 "income", "res_1980", "res_2002", "age_bir")
+nlsy <- read_csv(here::here("data", "raw", "nlsy.csv"),
+								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
+								 skip = 1, col_names = nlsy_cols) |>
+	mutate(region_cat = factor(region, labels = c("Northeast", "North Central", "South", "West")),
+				 sex_cat = factor(sex, labels = c("Male", "Female")),
+				 race_eth_cat = factor(race_eth, labels = c("Hispanic", "Black", "Non-Black, Non-Hispanic")),
+				 eyesight_cat = factor(eyesight, labels = c("Excellent", "Very good", "Good", "Fair", "Poor")),
+				 glasses_cat = factor(glasses, labels = c("No", "Yes")))
+
+
+# Customization of `tbl_summary()`
+
+tbl_summary(
+	nlsy,
+	by = sex_cat,
+	include = c(sex_cat, race_eth_cat, region_cat,
+							eyesight_cat, glasses, age_bir))
+
+
+tbl_summary(
+	nlsy,
+	by = sex_cat,
+	include = c(sex_cat, race_eth_cat, region_cat,
+							eyesight_cat, glasses, age_bir),
+	label = list(
+		race_eth_cat ~ "Race/ethnicity",
+		region_cat ~ "Region",
+		eyesight_cat ~ "Eyesight",
+		glasses ~ "Wears glasses",
+		age_bir ~ "Age at first birth"
+	),
+	missing_text = "Missing")
+
+
+tbl_summary(
+	nlsy,
+	by = sex_cat,
+	include = c(sex_cat, race_eth_cat,
+							eyesight_cat, glasses, age_bir),
+	label = list(
+		race_eth_cat ~ "Race/ethnicity",
+		eyesight_cat ~ "Eyesight",
+		glasses ~ "Wears glasses",
+		age_bir ~ "Age at first birth"
+	),
+	missing_text = "Missing") |>
+	add_p(test = list(all_continuous() ~ "t.test",
+										all_categorical() ~ "chisq.test")) |>
+	add_overall(col_label = "**Total**") |>
+	bold_labels() |>
+	modify_footnote(update = everything() ~ NA) |>
+	modify_header(label = "**Variable**", p.value = "**P**")
+
+#my code
+tbl_summary(
+	nlsy,
+	by = sex_cat,
+	include = c(region_cat, race_eth_cat, income, contains("sleep")),
+	label = list(region_cat ~ "Region",
+							 race_eth_cat ~ "Race/ethnicity",
+							 income ~ "Annual income",
+							 sleep_wkdy ~ "Hours of sleep during week",
+							 sleep_wknd ~ "Hours of sleep during weekend"),
+	statistic = list(income ~ "10% = {p10}; 90% = {p90}",
+									 sleep_wkdy ~ "min = {min}; max = {max}",
+									 sleep_wknd ~ "min = {min}; max = {max}"),
+	digits = list(income ~ 3,
+								sleep_wkdy ~ 0,
+								sleep_wknd ~ 0),
+	missing_text = "Missing") |>
+	add_p(test = list(all_continuous() ~ "t.test",
+										all_categorical() ~ "chisq.test"))|>
+	add_overall(col_label = "**Total**")
+
+	#modify_footnote_header("Please see: https://www.nlsinfo.org/content/cohorts/nlsy79/topical-guide/household/race-ethnicity-immigration-data for more information on how NLSY classified participants", columns = race_eth_cat)
+	#modify_footnote(update = race_eth_cat ~ "Please see: https://www.nlsinfo.org/content/cohorts/nlsy79/topical-guide/household/race-ethnicity-immigration-data for more information on how NLSY classified participants")
