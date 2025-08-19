920d1ee3a7f0d55630e8b712c105a2aad115f9ba
Brandie Shackelford <brandiebanner@gmail.com>
2025-08-19 14:26:35 -0400

    functions practice 1

---
+++ R/functions.R
@@ -0,0 +1,20 @@
+x <-3
+x^2
+
+square <- function(x){
+	second <- x*x
+	return(second)
+}
+
+square(x)
+square(53)
+
+53^2
+
+
+addtwonumbers <- function(y,z){
+	yplusz <- y+z
+	return(yplusz)
+}
+
+addtwonumbers(4,6)
